e7d7c211d6ec4043fd68f4dfb79b4eb9203d803d
Guiyom <gthvn1@gmail.com>
2024-11-17 16:57:22 +0100

    write magic instead of 61

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,8 @@
 open Core
 
 let () =
-  (* We want to create a file and write \x61 into it *)
+  (* We want to create a file and write the Qcow magic into it *)
+  let magic = Bytes.of_char_list [ 'Q'; 'F'; 'I'; '\xfb' ] in
   let oc = Out_channel.create "test.raw" in
-  Out_channel.output_byte oc 61;
+  Out_channel.output_bytes oc magic;
   print_endline @@ Qcow.Hello.say "world"
