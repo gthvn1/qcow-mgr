a23d39935aa990b4ab10c8123404746ee3e8553a
Guiyom <gthvn1@gmail.com>
2024-11-17 17:57:46 +0100

    fix test and minor update of the main

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,6 @@
 open Core
 
 let () =
-  (* We want to create a file and write the Qcow magic into it *)
-  let qh = Qcow.Header.create () in
+  let qhb = Qcow.Header.(create () |> to_bytes) in
   let oc = Out_channel.create "test.raw" in
-  Out_channel.output_bytes oc (Qcow.Header.to_bytes qh)
+  Out_channel.output_bytes oc qhb
--- test/test_qcow.ml
+++ test/test_qcow.ml
@@ -1,7 +1,11 @@
+let%test _ =
+  let qh = Qcow.Header.create () in
+  qh.magic = Int32.of_int 0x514649fb && qh.version = Int32.of_int 3
+
 let%test _ = 1 = 1
 
 let%expect_test _ =
-  print_endline @@ Qcow.Hello.say "Sailor";
+  print_endline "Hello, Sailor!";
   [%expect {|
-    Hello Sailor
+    Hello, Sailor!
   |}]
