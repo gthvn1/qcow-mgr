ea564d3622f31e0792c3a7df961f31cfff7a3747
Guiyom <gthvn1@gmail.com>
2024-11-17 16:07:48 +0100

    print a byte into a file

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline @@ Qcow.Hello.say "world"
+open Core
+
+let () =
+  (* We want to create a file and write \x61 into it *)
+  let oc = Out_channel.create "test.raw" in
+  Out_channel.output_byte oc 61;
+  print_endline @@ Qcow.Hello.say "world"
